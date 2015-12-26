76f54b548a313508bdc2dd5081e96fb56cf07380
Sanjo <Deundiak@gmail.com>
2015-12-26 17:11:05 +0200

    Fixed main

--- SA/Src/main.m
+++ SA/Src/main.m
@@ -1,16 +1,26 @@
 function [ R1, R2, R3, Y1, Y2, Y3 ] = process( startPos )
-%MAIN Summary of this function goes here
-%   Detailed explanation goes here
-
     N02 = 40;
     N03 = 10;
 
-    R1 = 1.0; R2 = 1.0; R3 = 1.0;
+    % Predict X
+    XaccVoltageKnown = data.data(startPos:startPos + N02 - 1, 3);
+    XcrankshaftKnown = data.data(startPos:startPos + N02 - 1, 4);
+    XadditionalGeneratorPowerKnown = data.data(startPos:startPos + N02 - 1, 5);
+    XconsumptionPowerKnown = data.data(startPos:startPos + N02 - 1, 6);
+
+    % Compute FZ
+    YgridVoltageKnown = data.data(startPos:startPos + N02 - 1, 7);
+    YfuelKnown = data.data(startPos:startPos + N02 - 1, 8);
+    YaccVoltageKnown = data.data(startPos:startPos + N02 - 1, 9);
+
+    % Predict Y
     
-    Y1 = data.data(startPos:startPos + N02 + N03, 7);
-    Y2 = data.data(startPos:startPos + N02 + N03, 8);
-    Y3 = data.data(startPos:startPos + N02 + N03, 9);
-end
+    
+    R1 = 1.0; R2 = 1.0; R3 = 1.0;
+
+    Y1 = data.data(startPos:startPos + N02 + N03 - 1, 7);
+    Y2 = data.data(startPos:startPos + N02 + N03 - 1, 8);
+    Y3 = data.data(startPos:startPos + N02 + N03 - 1, 9);
 
 function [ ] = filenameChangeNotify ( filename ) 
     data = importData(filename);
