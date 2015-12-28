cf7ef6960f9008b281544db87b635aa3107ac66d
ergleb <ergleb23@gmail.com>
2015-12-28 05:46:51 +0200

    tried to fix functional dependencies:(

--- SA/Src/ArimaP.m
+++ SA/Src/ArimaP.m
@@ -1,7 +1,10 @@
 function [ res ] = ArimaP( data,K )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
- Mdl = arima(); 
+global Mdl;
+if isempty(Mdl)
+ Mdl = arima();
+end
    y=data;
 %past_data = z1(1:40);
 %future_inputs = z1(41:end);
